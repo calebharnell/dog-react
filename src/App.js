b0c35caab89267c91968ded4c7d9112106d6d45f
calebharnell <calebharnell@hotmail.com>
2017-11-30 11:46:06 +1000

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   state = {
     dogs: ['Jeff', 'Scooby']
   }
+
   render() {
     return (
       <div className="App">
@@ -15,6 +16,22 @@ class App extends Component {
       </div>
     );
   }
+
+  componentDidMount() {
+    // Grab our dogs from the API
+    // Make a request for a user with a given ID
+    axios.get('/api/dogs')
+      .then(function (response) {
+        console.log('Success!')
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log('Whoops!')
+        console.log(error);
+      });
+
+  }
+
 }
 
 export default App;
